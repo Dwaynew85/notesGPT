31743f2a1ecf978e1b096b3141db903516dfde69
Dwaynew85 <mr_dwayne_w@yahoo.com>
2025-03-23 02:23:56 -0400

    header including sign up, login, and dark mode buttons

---
+++ src/components/Header.tsx
@@ -0,0 +1,48 @@
+import { shadow } from "@/app/styles/utils"
+import Image from "next/image"
+import Link from "next/link"
+import { Button } from "./ui/button";
+import DarkModeToggle from "./ui/darkmodetoggle";
+
+const Header = () => {
+  const user = null;
+
+  return (
+    <header className="bg-popover relative flex h-24 w-full items-center justify-between bg-popover px-3 sm:px-8"
+      style={{
+        boxShadow: shadow,
+      }}
+    >
+      <Link className="flex items-end gap-2" href="/">
+        <Image
+          className="rounded-full"
+          src="/logo.png"
+          alt="logo"
+          width={60}
+          height={60}
+          priority
+        />
+        <h1 className="flex flex-col pb-1 text-2xl font-semibold leading-6">Notes <span>GPT</span></h1>
+      </Link>
+
+      <div className="flex gap-4">
+        {user ? (
+          "Logout"
+        ) : (
+          <>
+            <Button asChild>
+              <Link href="/sign-up" className="hidden sm:block">Sign Up</Link>
+            </Button>
+            <Button asChild variant="outline">
+              <Link href="/login">Login</Link>
+            </Button>
+          </>
+        )}
+        <DarkModeToggle />
+      </div>
+
+    </header>
+  )
+}
+
+export default Header
